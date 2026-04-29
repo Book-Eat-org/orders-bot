afc4b18eb88aeaa61c4c559d744f346d09d8a4aa
Leo Kooper <lverixas@gmail.com>
2026-04-29 21:06:08 +0300

    BOOKEATKANBAN-446 Исправить доступность бота (мтпрото или аналоги) feat: добавлен конфиг проксирования

--- src/app/settings.py
+++ src/app/settings.py
@@ -3,12 +3,11 @@ from functools import cache as cache_func
 
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     BOT: str = os.environ.get('BOT_API_TOKEN')
     EXTERNAL_API_URL: str = os.environ.get('EXTERNAL_API_URL')
     EXTERNAL_API_CHECK_ACCESS: str = os.environ.get('EXTERNAL_API_CHECK_ACCESS')
-
+    PROXY_URL: str = os.environ.get("PROXY_URL", "")
 
 @cache_func
 def get_settings() -> Settings:
